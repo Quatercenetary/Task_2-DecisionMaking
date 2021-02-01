970985010c206640f91d84432dc4c6fec8f3707c
Quatercenetary <72935043+Quatercenetary@users.noreply.github.com>
2021-02-02 03:17:56 +0600

    Created EligibleForVotingOrNot
    This file tells if you are eligible for voting. (Be sure to put a valid age. If you don't... well, it will increase your work, anyways)

---
+++ EligibleForVotingOrNot.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age;
+    LOOP:
+    do{
+        cout<<"Enter your age: ";
+        cin>>age;
+        if(age < 0) cout<<"Not a valid age. Try again."<<endl;
+        else break;
+    }while( age < 0);
+
+    if (age >= 18 && age <= 120) 
+        cout<<"You are eligible to vote"<<endl;
+    else if( age >=0 && age < 18)
+        cout<<"You are not eligible to vote."<<endl;
+    else{
+        char str = 'Y';
+        int i = 0;
+        while(str == 'Y' || str == 'y' && i <= 5){
+            if(i < 3) cout<<"Really? (Reply in 'y' or 'n' only): ";
+            else cout<<"Are you sure? (You know the drill): ";
+            cin>>str;
+            i++;
+        }
+        if(str == 'N' || str == 'n') {
+            cout<<"Cool, try again, later."<<endl;
+            goto LOOP;
+        }
+        else if(str == 'Y' || str == 'y') cout<<"Fine! Sure You can vote."<<endl;
+        else {
+            cout<<"I said, 'y' or 'n' only! Try again."<<endl;
+            goto LOOP;
+        }
+    }
+    return 0;
+}
